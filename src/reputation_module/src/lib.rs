51f48beeda98b5f681f14e07f962a6be99c859c4
Leontiy <leosungun@gmail.com>
2024-06-02 19:13:34 +0300

    reputation module permissions

--- src/reputation_module/src/lib.rs
+++ src/reputation_module/src/lib.rs
@@ -51,9 +51,31 @@ thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 
-    static ALLOWED_CANISTER: RefCell<StableBTreeMap<CanisterPrincipal, CanisterPermission, Memory>> = RefCell::new(
+    static CANISTERS_PERSMISSION: RefCell<StableBTreeMap<CanisterPrincipal, CanisterPermission, Memory>> = RefCell::new(
         StableBTreeMap::init(
             MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
         )
     );
+}
+
+#[query(name = "isController")]
+fn is_controller(id: Principal) -> bool {
+    let id = ic_cdk::api::caller();
+    let is_controller = ic_cdk::api::is_controller(&id);
+
+    return is_controller;
+}
+
+
+#[query(name = "changePemissionCanister")]
+fn change_permission_canister(canister: Principal, permission: bool) -> Result<String, String> {
+    let id = ic_cdk::api::caller();
+    let is_controller = ic_cdk::api::is_controller(&id);
+
+    if is_controller {
+        CANISTERS_PERSMISSION.with(|p| p.borrow_mut().insert(CanisterPrincipal(canister), CanisterPermission(permission)));
+        Ok(String::from("Granted permissions to canister"))
+    } else {
+        Ok(String::from("Access denied"))
+    }
 }
