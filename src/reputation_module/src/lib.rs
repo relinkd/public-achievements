a8343926e786299d3da51a8e93eeb12fa05d7b9f
Leontiy <leosungun@gmail.com>
2024-06-02 17:38:55 +0300

    init reputation_module

---
+++ src/reputation_module/src/lib.rs
@@ -0,0 +1,11 @@
+use ic_cdk::{query};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{
+    storable::Bound, DefaultMemoryImpl, StableBTreeMap, Storable,
+};
+use std::{borrow::Cow, cell::RefCell};
+
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+}
