68f7346d62cc75d72ed2e16587ea116f3be81257
Leontiy <leosungun@gmail.com>
2024-06-09 13:48:06 +0300

    reputation module add issue_achievement_to_identity_wallet method

--- src/reputation_module/src/lib.rs
+++ src/reputation_module/src/lib.rs
@@ -14,7 +14,7 @@ thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 
-    static ACHIEVEMENT_CANISTER_TO_ID: RefCell<StableBTreeMap<CanisterPrincipal, CanisterPermission, Memory>> = RefCell::new(
+    static ACHIEVEMENT_CANISTER_TO_BOOL: RefCell<StableBTreeMap<CanisterPrincipal, CanisterPermission, Memory>> = RefCell::new(
         StableBTreeMap::init(
             MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
         )
@@ -50,7 +50,7 @@ fn change_permission_canister(canister: Principal, permission: bool) -> Result<S
     let is_controller = ic_cdk::api::is_controller(&id);
 
     if is_controller {
-        ACHIEVEMENT_CANISTER_TO_ID.with(|p| p.borrow_mut().insert(CanisterPrincipal(canister), CanisterPermission(permission)));
+        ACHIEVEMENT_CANISTER_TO_BOOL.with(|p| p.borrow_mut().insert(CanisterPrincipal(canister), CanisterPermission(permission)));
         Ok(String::from("Granted permissions to canister"))
     } else {
         Err(String::from("Access denied"))
@@ -59,10 +59,29 @@ fn change_permission_canister(canister: Principal, permission: bool) -> Result<S
 
 #[query(name = "isCanisterAllowed")]
 fn is_canister_allowed(canister: Principal) -> Result<CanisterPermission, String> {
-    if let Some(permission) = ACHIEVEMENT_CANISTER_TO_ID.with(|p| p.borrow().get(&CanisterPrincipal(canister))) {
+    if let Some(permission) = ACHIEVEMENT_CANISTER_TO_BOOL.with(|p| p.borrow().get(&CanisterPrincipal(canister))) {
         Ok(permission)
     } else {
         Err(String::from("Canister not found"))
+    }
+}
+
+#[query(name = "issueAchievementToIdentityWallet")]
+async fn issue_achievement_to_identity_wallet(achievement: Principal) -> Result<String, String> {
+    let canister_permission = is_canister_allowed(achievement)?;
+
+    if !canister_permission.0 {
+        return Err(String::from("Achievement not allowed"));
+    }
+
+    let caller = ic_cdk::api::caller();
+    let status: (u8, ) = ic_cdk::call(achievement, "getPrincipalToAchievementStatusValue", (caller,)).await.unwrap();
+
+    if status.0 == 1_u8 {
+        // your logic to issue achievement
+        Ok(String::from("Achievement issued"))
+    } else {
+        Err(String::from("You`re not allowed"))
     }
 }
 
